119c5423cff52538efeabd54115e8a64c2657594
FalconPilot <fpilot.github@gmail.com>
2019-03-10 01:58:26 +0100

    fixed countries api

--- src/router/api/index.js
+++ src/router/api/index.js
@@ -1,7 +1,9 @@
 const weaponsRouter = require('./weapons')
+const countriesRouter = require('./countries')
 
 const router = require('express').Router()
 
 router.use('/weapons', weaponsRouter)
+router.use('/countries', countriesRouter)
 
 module.exports = router
--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,12 @@ const fs = require('fs-extra')
 
 const appRouter = require('./app')
 const apiRouter = require('./api')
-const countriesRouter = require('./countries')
 
 const mainRouter = app => {
 
   // Plug routers
   app.use('/', appRouter)
   app.use('/api', apiRouter)
-  app.use('/countries', countriesRouter)
 }
 
 module.exports = mainRouter
